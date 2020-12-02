18379a8079ce71cf0fa08ad2f8ad15b02da4765a
Kaishiyoku <dmp@nfze.de>
2020-12-02 20:29:57 +0100

    added Windows compatibility
    (cherry picked from commit 982f506f9f67a50d8e5c2fa116c1121f4be20f1d)

--- src/utils/readInput.js
+++ src/utils/readInput.js
@@ -1,9 +1,12 @@
+const os = require('os');
 const { readFileSync } = require("fs")
 const getCallerFile = require("get-caller-file")
 
+const splitChar = os.platform() === "win32" ? "\\" : "/";
+
 const readInput = () => {
   const file = getCallerFile()
-    .split("/")
+    .split(splitChar)
     .slice(0, -1)
     .concat("input.txt")
     .join("/")
